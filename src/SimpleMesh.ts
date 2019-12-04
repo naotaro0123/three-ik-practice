0d63dea886d19f211cd6f5da2657ad8577a2a6f8
naotaro0123 <n.ikewada@gmail.com>
2019-12-05 02:09:53 +0900

    fix: add gizmo, grid, SkinnedMesh

--- src/SimpleMesh.ts
+++ src/SimpleMesh.ts
@@ -1,48 +1,67 @@
 import * as THREE from 'three';
 import { IK, IKChain, IKJoint, IKBallConstraint, IKHelper } from 'three-ik';
+const TransformControls = require('three-transform-controls')(THREE);
+import { OrbitControls } from 'three-orbitcontrols-ts';
 
-const MAX_JOINTS = 7;
+const MAX_JOINTS = 3;
+const CAMERA_POS_Z = 3;
+const TARGET_POS_Z = 1;
+const TARGET_POS_Y = 0.2;
 
 class SimpleMesh {
   private renderer: THREE.WebGLRenderer;
   private camera: THREE.PerspectiveCamera;
   private scene: THREE.Scene;
-  private ik: IK;
-  private pivot: THREE.Object3D;
+  private controls: OrbitControls;
+  private gizmos: any[] = [];
 
   constructor() {
     this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xeeeeee);
     this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 100);
-    this.camera.position.z = 5;
+    this.camera.position.z = CAMERA_POS_Z;
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(this.renderer.domElement);
 
-    this.initIK();
-    this.render();
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    const light = new THREE.AmbientLight(0xFFFFFF, 1.0);
+    this.scene.add(light);
+
+    const grid = new THREE.GridHelper(20, 20, 0x000000, 0x000000);
+    this.scene.add(grid);
+
+    const [ik, pivot] = this.initIK();
+
+    this.render(ik, pivot);
   }
 
-  initIK() {
-    this.ik = new IK();
-    this.ik.isIK = true;
-    const movingTarget = new THREE.Mesh(
-      new THREE.SphereBufferGeometry(0.1),
-      new THREE.MeshBasicMaterial({ color: 0xff0000 })
-    );
+  private initIK(): [IK, THREE.Object3D] {
+    const movingTarget = this.createTarget(new THREE.Vector3());
+    movingTarget.position.z = TARGET_POS_Z;
+    movingTarget.position.y = TARGET_POS_Y;
+
+    const pivot = new THREE.Object3D();
+    pivot.add(movingTarget);
+    this.scene.add(pivot);
+
+    const ik = new IK();
+    ik.isIK = true;
+
+    const [chain, bones] = this.createBonesAndChain(movingTarget);
+    this.createMeshs(bones);
+    
+    ik.add(chain);
+    this.scene.add(ik.getRootBone());
+    this.createHelper(ik);
+
+    return [ik, pivot];
+  }
+
+  private createBonesAndChain(movingTarget: THREE.Object3D): [ IKChain, THREE.Bone[]] {
     const chain = new IKChain();
-    movingTarget.position.z = 2;
-    this.pivot = new THREE.Object3D();
-    this.pivot.add(movingTarget);
-    this.scene.add(this.pivot);
-
-    this.createBones(chain, movingTarget);
-    this.ik.add(chain);
-    this.scene.add(this.ik.getRootBone());
-    this.createHelper();
-  }
-
-  createBones(chain: IKChain, movingTarget: THREE.Mesh) {
     const constraints = [new IKBallConstraint(90)];
     const bones: THREE.Bone[] = [];
     
@@ -57,20 +76,60 @@ class SimpleMesh {
       const target = i === MAX_JOINTS - 1 ? movingTarget : null;
       chain.add(new IKJoint(bone, { constraints }), { target });
     }
+    return [chain, bones];
   }
 
-  createHelper() {
-    const helper = new IKHelper(this.ik);
+  private createMeshs(bones: THREE.Bone[]) {
+    console.log(bones)
+    const geometry = new THREE.CylinderGeometry(0.2, 0.2, 0.5, 10);
+    const material = new THREE.MeshPhongMaterial({
+      skinning: true,
+      color: 0x156289,
+      emissive: 0x072534,
+      side: THREE.DoubleSide,
+      transparent: true,
+      opacity: 0.5
+    });
+    const mesh = new THREE.SkinnedMesh(geometry, material);
+    const skelton = new THREE.Skeleton(bones);
+    mesh.add(bones[0]);
+    mesh.bind(skelton);
+    this.scene.add(mesh);
+  }
+
+  createTarget(position: THREE.Vector3) {
+    const gizmo = new TransformControls(
+      this.camera, 
+      this.renderer.domElement
+    );
+    const target = new THREE.Object3D();
+    gizmo.setSize(1.0);
+    gizmo.attach(target);
+    gizmo.target = target;
+    target.position.copy(position);
+    this.scene.add(gizmo);
+    this.scene.add(target);
+    this.gizmos.push(gizmo);
+
+    return target;
+  }
+
+  private createHelper(ik: IK) {
+    const helper = new IKHelper(ik);
     this.scene.add(helper);
   }
 
-  render() {
-    this.pivot.rotation.x += 0.01;
-    this.pivot.rotation.y += 0.01;
-    this.pivot.rotation.z += 0.01;
-    this.ik.solve();
+  private render(ik:IK, pivot: THREE.Object3D) {
+    ik.solve();
+
+    this.controls.update();
+
+    for (let gizmo of this.gizmos) {
+      gizmo.update();
+    }
+
     this.renderer.render(this.scene, this.camera);
-    requestAnimationFrame(() => this.render());
+    requestAnimationFrame(() => this.render(ik, pivot));
   }
 }
 
